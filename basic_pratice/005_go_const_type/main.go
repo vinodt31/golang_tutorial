584c92ac51471573510aa0d0d97f44ac859c9622
vinodt31 <vinodt31@gmail.com>
2026-05-23 23:48:22 +0530

    go constant type pratice

---
+++ basic_pratice/005_go_const_type/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	const aapName string = "Go basic application"
+
+	const uploadMaxSize int = 20
+
+	const discountPrice float64 = 0.5
+
+	fmt.Println(aapName, uploadMaxSize, discountPrice)
+
+
+}
