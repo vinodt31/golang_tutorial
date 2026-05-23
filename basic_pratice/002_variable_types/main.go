b356b118cbd02abf720548271ec56a7d53f32a96
vinodt31 <vinodt31@gmail.com>
2026-05-23 17:34:30 +0530

    variable types pratice

---
+++ basic_pratice/002_variable_types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "vinod kumar tiwari"
+	var age int = 30;
+	var rating float64 = 4.5;
+	var isActive bool = true;
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age: ", age)
+	fmt.Println("rating: ", rating)
+	fmt.Println("Is Active: ", isActive)
+}
