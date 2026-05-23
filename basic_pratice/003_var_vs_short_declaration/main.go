83a7e8b33ba49a44c4b1a5f5e2c27fa72effba90
vinodt31 <vinodt31@gmail.com>
2026-05-23 21:43:02 +0530

    var vs shor declaration example

---
+++ basic_pratice/003_var_vs_short_declaration/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var name string = "vinod kumar tiwari"
+
+	price := 100
+	price = price + 200
+
+	product, qty := "mobile", 2
+
+	fmt.Println(name, price, product, qty)
+
+
+}
